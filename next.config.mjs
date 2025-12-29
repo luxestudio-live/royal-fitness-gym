5944dd52814b5296408e8b10496d7cc2e718420c
Rahul Kushwaha <rk.rahulkushwaha22@gmail.com>
2025-12-29 13:04:47 +0530

    Update next.config.mjs for Royal Fitness Gym GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'export',
-  assetPrefix: process.env.GITHUB_PAGES === 'true' ? '/bulldog-gym/' : '',
-  basePath: process.env.GITHUB_PAGES === 'true' ? '/bulldog-gym' : '',
+  assetPrefix: process.env.GITHUB_PAGES === 'true' ? '/royal-fitness-gym/' : '',
+  basePath: process.env.GITHUB_PAGES === 'true' ? '/royal-fitness-gym' : '',
 }
 
 export default nextConfig
